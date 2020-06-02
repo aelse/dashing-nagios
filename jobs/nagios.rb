48602539b0a3b932a326c254c22c38d4eb8d90f1
Alexander Else <aelse@else.id.au>
2020-06-02 10:22:20 +1000

    Pass version and datetime format to nagiosharder
    Users report in #3 and #4 that passing nagios version 3 and 'iso8601' to nagiosharder make the data collection job work. I haven't used this module in years and i'm unable to test it, but making the change in case this smooths the road for anyone in future.

--- jobs/nagios.rb
+++ jobs/nagios.rb
@@ -8,7 +8,7 @@ SCHEDULER.every '30s' do
   }
 
   environments.each do |key, env|
-    nag = NagiosHarder::Site.new(env[:url], env[:username], env[:password])
+    nag = NagiosHarder::Site.new(env[:url], env[:username], env[:password], 3, 'iso8601')
     unacked = nag.service_status(:host_status_types => [:all], :service_status_types => [:warning, :critical], :service_props => [:no_scheduled_downtime, :state_unacknowledged, :checks_enabled])
 
     critical_count = 0
